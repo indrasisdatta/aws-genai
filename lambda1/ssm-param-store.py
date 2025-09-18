879e5de1c1bf9ec5990747482bc8053a4af95cc3
Indrasis Datta <indrasis.datta@yahoo.co.in>
2025-09-18 08:40:10 +0530

    SSM param store test lambda function

--- lambda1/ssm-param-store.py
+++ lambda1/ssm-param-store.py
@@ -1,6 +1,14 @@
 import boto3 
+import os 
 
-ssm = boto3.client('ssm', region_name='eu-west-1')
+# ssm = boto3.client('ssm', region_name='eu-west-1')
+
+# Test code
+session = boto3.Session(profile_name=os.getenv('AWS_PROFILE'), region_name="ap-south-1")
+# session = boto3.Session(profile_name="awsapp-role-ap696l85", region_name="ap-south-1")
+ssm = session.client('ssm')
+params = ssm.get_parameters(Names=['MY_AWS_S3_BUCKET'])
+print('SSM params:', params)
 
 def lambda_handler(event, context):
     print(event)
